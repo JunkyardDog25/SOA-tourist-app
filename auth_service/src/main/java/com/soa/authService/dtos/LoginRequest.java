99398de242cf07e38aa70e95b6e56257ce139497
NemanjaR <nemanjaradic234@gmail.com>
2026-03-30 21:55:25 +0200

    user auth implemented

---
+++ auth_service/src/main/java/com/soa/authService/dtos/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.soa.authService.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank private String email;
+    @NotBlank private String password;
+}
---
+++ auth_service/src/main/java/com/soa/authService/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.soa.authService.services;
+
+import com.soa.authService.models.User;
+import com.soa.authService.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(usernameOrEmail)
+                .or(() -> userRepository.findByUsername(usernameOrEmail))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + usernameOrEmail));
+
+        return user;
+    }
+}
