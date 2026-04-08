9e1e1ce17c1ac7611f36cdecc8c9b416071af54a
isidor <isidor@ISIDOR>
2026-04-08 18:48:35 +0200

    Izmena profila korisnika implementirano

--- stakeholders_service/src/main/java/com/soa/stakeholdersService/controllers/UserController.java
+++ stakeholders_service/src/main/java/com/soa/stakeholdersService/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.soa.stakeholdersService.controllers;
 
 import com.soa.stakeholdersService.dtos.ProfileResponseDto;
+import com.soa.stakeholdersService.dtos.UpdateProfileDto;
 import com.soa.stakeholdersService.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,9 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<ProfileResponseDto> getMyProfile() {
         return ResponseEntity.ok(userService.getMyProfile());
+    }
+    @PatchMapping("/me")
+    public ResponseEntity<ProfileResponseDto> updateMyProfile(@RequestBody UpdateProfileDto dto) {
+        return ResponseEntity.ok(userService.updateMyProfile(dto));
     }
 }
--- stakeholders_service/src/main/java/com/soa/stakeholdersService/dtos/ProfileResponseDto.java
+++ stakeholders_service/src/main/java/com/soa/stakeholdersService/dtos/ProfileResponseDto.java
@@ -14,3 +14,20 @@ public class ProfileResponseDto {
     private String biography;
     private String motto;
 }
+//Ručno dodavanje korisnika u bazu podataka (Neo4j) za testiranje profila korisnika
+//Ovaj Cypher upit kreira čvor "User" sa svim potrebnim atributima za testiranje funkcionalnosti profila korisnika u aplikaciji.
+/*
+CREATE (u:User {
+    id: randomUUID(),
+    authUserId: "52403a1c-9923-4622-af14-08193e5aafcd",
+    username: "Isidor2004",
+    email: "isidorivanov061@gmail.com",
+    role: "GUIDE",
+    firstName: "Isidor",
+    lastName: "Ivanov",
+    profileImageUrl: "https://example.com/avatar.jpg",
+    biography: "Iskusni vodič sa 5 godina iskustva.",
+    motto: "Putuj, uči, rasti."
+})
+RETURN u
+*/
