2d81929279e173da4375fd808fd4e11324242c42
NemanjaR <nemanjaradic234@gmail.com>
2026-05-03 23:45:25 +0200

    changed tech for followers service

--- followers_service/go.mod
+++
@@ -1,3 +0,0 @@
-module followers_service
-
-go 1.23
---
+++ followers_service/src/main/java/com/soa/followersService/FollowersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.soa.followersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowersServiceApplication.class, args);
+	}
+
+}
